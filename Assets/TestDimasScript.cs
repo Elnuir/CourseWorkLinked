70e5422dd2a80d4a2e0256f38f017e9ea2df1a2f
Elnuir <66650896+Elnuir@users.noreply.github.com>
2020-11-28 00:57:36 +0300

    start
    start

---
+++ Assets/TestDimasScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDimasScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform RightHandObj;
+    private Animator animator;
+    private float value = 0.0f;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+      
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RightHandObj.transform.position = new Vector3(value, 0, 0);
+        value += 0.1f;
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+      
+    }
+}
---
+++ Assets/Trash/force.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class force : MonoBehaviour
+{
+Rigidbody2D circle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        circle = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        circle.velocity = new Vector2(20, 0);
+    }
+}
